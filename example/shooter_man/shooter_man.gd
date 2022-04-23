124209012df3ece1ec17af5865d5bda95e429e3a
FireSquid <jdeiss06@gmail.com>
2022-04-23 15:16:30 -0500

    update shooter

--- example/shooter_man/shooter_man.gd
+++ example/shooter_man/shooter_man.gd
@@ -1,4 +1,5 @@
 extends KinematicBody2D
+class_name ShooterPlayer
 
 
 export var move_spd = 400 # move spd in pixels/second
@@ -6,6 +7,6 @@ var move_dir
 
 
 func _physics_process(delta):
-	move_dir = Vector2(int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")), int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up")))
+	move_dir = Vector2(int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")), int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))).normalized()
 
 	move_and_slide(move_dir * move_spd)
