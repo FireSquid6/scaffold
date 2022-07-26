860b9a5519a309f262d0a7b1ed22946da5790082
Jonathan Deiss <39541470+FireSquid6@users.noreply.github.com>
2022-07-26 17:34:30 -0500

    rename object saver to object freezer

--- example/save_data/data_saver.gd
+++ example/save_data/data_saver.gd
@@ -5,15 +5,25 @@ const save_location = "user://data.save"
 
 
 func _ready():
+	# feel free to throw a breakpoint here!
+	# run data_saver.tscn to run this script
+	
+	# create a new instance of save data and then modify it
 	var data = SaveData.new()
 	data.lives = 9
 	data.level = 10
 	data.name = "Jonathan"
 	data.position = Vector2(1205, -1237)
 	
-	ObjectSaver.save_object(save_location, data)
+	# freeze the properties of save data
+	ObjectFreezer.freeze_object(save_location, data)
 	
+	# create a new save data object
 	var new_data: SaveData = SaveData.new()
-	ObjectSaver.load_object(save_location, new_data)
+	
+	# load the properties of the frozen object onto the new one
+	ObjectFreezer.microwave_object(save_location, new_data)
+	
+	# print the properties of the new object
 	print(new_data.name)
 	print(new_data.position)
